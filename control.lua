f1df82f4f70f922a6e9562520e8e46074c934f39
Choumiko <ghostface@ghostface.info>
2015-03-24 22:05:39 +0100

    refactor

--- control.lua
+++ control.lua
@@ -1,23 +1,31 @@
-inserters = {
-  basic = "basic-inserter",
-  fast  = "fast-inserter",
-  smart = "smart-inserter",
-  long = "long-handed-inserter"
-}
-assemblers = {
-  am1 = "assembling-machine-1",
-  am2 = "assembling-machine-2",
-  am3 = "assembling-machine-3"
-}
-belts = {
-  basic = "basic-",
-  fast = "fast-",
-  express = "express-"
-}
-beltTypes = {
-  underground = "transport-belt-to-ground",
-  splitter = "splitter",
-  belt = "transport-belt"
+
+replaceableEntities = {
+  inserter = {
+    basic = "basic-inserter",
+    fast  = "fast-inserter",
+    smart = "smart-inserter",
+    long = "long-handed-inserter"
+  },
+  assembler = {
+    am1 = "assembling-machine-1",
+    am2 = "assembling-machine-2",
+    am3 = "assembling-machine-3"
+  },
+  belt = {
+    basic = "basic-transport-belt",
+    fast = "fast-transport-belt",
+    express = "express-transport-belt"
+  },
+  underground = {
+    basic = "basic-transport-belt-to-ground",
+    fast = "fast-transport-belt-to-ground",
+    express = "express-transport-belt-to-ground"
+  },
+  splitter = {
+    basic = "basic-splitter",
+    fast = "fast-splitter",
+    express = "express-splitter"
+  }
 }
 
 BpMirror = {
@@ -43,7 +51,8 @@ BpMirror = {
     return blueprints
   end,
 
-  findSetupBlueprintInHotbar = function(player)
+  findSetupBlueprintInHotbar = function(name)
+    local player = getPlayerByName(name)
     local blueprints = BpMirror.findBlueprintsInHotbar(player)
     if blueprints ~= nil then
       for i, blueprint in ipairs(blueprints) do
@@ -68,16 +77,14 @@ function debugDump(var, force)
     end
   end
 end
---function saveVar(var, name)
---  local var = var or glob
---  local n = name or ""
---  game.makefile("bp/bp"..n..".lua", serpent.block(var, {name="glob"}))
---  --game.makefile("farl/loco"..n..".lua", serpent.block(findAllEntitiesByType("locomotive")))
---end
+function saveVar(var, name)
+  local var = var or glob
+  local n = name or ""
+  game.makefile("bp/bp"..n..".lua", serpent.block(var, {name="glob"}))
+  --game.makefile("farl/loco"..n..".lua", serpent.block(findAllEntitiesByType("locomotive")))
+end
 
-function mirror(bp, args)
-  local axis = args[1]
-  local entities = bp.getblueprintentities()
+function mirror(entities, axis)
   local dirs = {x ={0,4},y={2,6}}
   for _,ent in pairs(entities) do
     ent.position[axis] = -1 * ent.position[axis]
@@ -88,7 +95,7 @@ function mirror(bp, args)
   return entities
 end
 
-function getPlayerID(name)
+function getPlayerByName(name)
   if not name and #game.players > 1 then
     for id, player in pairs(game.players) do
       player.print("Use /c remote.call(\"bpmirror\", \"mirrorV\", \""..player.name.."\")")
@@ -104,98 +111,137 @@ function getPlayerID(name)
       end
     end
   end
-  return id
+  return game.players[id]
 end
 
 function manipulate(name, func, args)
-  local id = getPlayerID(name)
-  if id then
-    local bp = BpMirror.findSetupBlueprintInHotbar(game.players[id])
-    if bp then
-      local mir = func(bp, args)
-      bp.setblueprintentities(mir)
-    end
+  local bp = BpMirror.findSetupBlueprintInHotbar(name)
+  if bp then
+    local mir = func(bp, args)
+    bp.setblueprintentities(mir)
   end
 end
 
-function replace(bp, args)
-  local s = args[1]
-  local r = args[2]
-  local entities = bp.getblueprintentities()
-  for _,ent in pairs(entities) do
+function replace(entities, type, s, r)
+  local n = entities
+  if not replaceableEntities[type] then
+    return "Unknown type: "..type
+  end
+  if not replaceableEntities[type][s] then
+    return "Unknown "..type.." "..s
+  end
+  if not replaceableEntities[type][r] then
+    return "Unknown "..type.." "..r
+  end
+  local s = replaceableEntities[type][s]
+  local r = replaceableEntities[type][r]
+  for _,ent in pairs(n) do
     if ent.name == s then
       ent.name = r
     end
   end
-  return entities
+  return n
+end
+
+function changeRecipe(entities, s, r)
+  for _,ent in pairs(n) do
+    if ent.recipe and ent.recipe == s then
+      ent.recipe = r
+    end
+  end
+  return n
+end
+
+function globalPrint(msg)
+  for _,p in pairs(game.players) do
+    p.print(msg)
+  end
+end
+
+function printByName(name, msg)
+  for _,p in pairs(game.players) do
+    if p.name == name then
+      p.print(msg)
+      return
+    end
+  end
+  game.players[1].print(msg)
 end
 
 remote.addinterface("bpmirror",
   {
     mirrorV = function(name)
-      manipulate(name,mirror,{"y"})
+      local bp = BpMirror.findSetupBlueprintInHotbar(name)
+      if bp then
+        local mir = mirror(bp.getblueprintentities, "y")
+        bp.setblueprintentities(mir)
+      end
     end,
-    
+
     mirrorH = function(name)
-      manipulate(name,mirror,{"x"})
+      local bp = BpMirror.findSetupBlueprintInHotbar(name)
+      if bp then
+        local mir = mirror(bp.getblueprintentities, "x")
+        bp.setblueprintentities(mir)
+      end
     end,
-    
+
     replaceBelts = function(s,r, name)
-      local id = getPlayerID(name)
-      local error = false
-      if not belts[s] then
-        game.players[id].print("Unknown belt-type: "..s)
-        error=true
-      end
-      if not belts[r] then
-        game.players[id].print("Unknown belt-type: "..r)
-        error=true
-      end
-      if error then return end
-      if id then
-        local bp = BpMirror.findSetupBlueprintInHotbar(game.players[id])
-        if bp then
-          for k,type in pairs(beltTypes) do
-            local search = belts[s]..type
-            local rep = belts[r]..type
-            local ent = replace(bp, {search, rep})
-            bp.setblueprintentities(ent)
+      local belts = {"belt", "underground", "splitter"}
+      local bp = BpMirror.findSetupBlueprintInHotbar(name)
+      if bp then
+        local new = bp.getblueprintentities()
+        for _,t in pairs(belts) do
+          new = replace(new,t,s,r)
+          if type(new) == "table" then
+            bp.setblueprintentities(new)
+          else
+            printByName(name,new)
           end
         end
+      else
+        printByName(name,"No blueprint found")
       end
     end,
-    
+
     replaceInserters = function(s,r,name)
-    local id = getPlayerID(name)
-      local error = false
-      if not inserters[s] then
-        game.players[id].print("Unknown inserter-type: "..s)
-        error=true
+      local bp = BpMirror.findSetupBlueprintInHotbar(name)
+      local ent = bp.getblueprintentities()
+      local new = replace(ent,"inserter",s,r)
+      if type(new) == "table" then
+        bp.setblueprintentities(new)
+      else
+        printByName(name,new)
       end
-      if not inserters[r] then
-        game.players[id].print("Unknown inserter-type: "..r)
-        error=true
-      end
-      if error then return end
-      manipulate(name,replace,{inserters[s], inserters[r]})
     end,
-    
+
     replaceAssemblers = function(s,r,name)
-    local id = getPlayerID(name)
-      local error = false
-      if not assemblers[s] then
-        game.players[id].print("Unknown assembler-type: "..s)
-        error=true
+      local bp = BpMirror.findSetupBlueprintInHotbar(name)
+      local ent = bp.getblueprintentities()
+      local new = replace(ent,"assembler",s,r)
+      if type(new) == "table" then
+        bp.setblueprintentities(new)
+      else
+        printByName(name,new)
       end
-      if not assemblers[r] then
-        game.players[id].print("Unknown assembler-type: "..r)
-        error=true
-      end
-      if error then return end
-      manipulate(name,replace,{assemblers[s], assemblers[r]})
     end,
-        
-    replace = function(s, r, name)
-      manipulate(name,replace,{s, r})
+
+    addEntity = function(type, key, name)
+      local reserved = {belt={"basic", "fast", "express"}}
+      reserved.underground = reserved.belt
+      reserved.splitter = reserved.belt
+      reserved.assembler = {"am1", "am2", "am3"}
+      reserved.inserter = {"basic", "fast", "smart", "long"}
+      for _,k in pairs(reserved[type]) do
+        if k == key then
+          globalPrint(key.." is reserved for type "..type)
+          return
+        end
+      end
+      replaceableEntities[type][key] = name
+    end,
+
+    saveBP = function(name)
+      saveVar(BpMirror.findSetupBlueprintInHotbar(name).getblueprintentities())
     end
   })
